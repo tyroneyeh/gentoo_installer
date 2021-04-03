475a984eefa7a69132d59ef10c762bc6ad3acbcb
Tyrone Yeh <tyrone_yeh@draytek.com>
2021-04-03 22:44:43 +0800

    Adjust something and add short url.

--- install_gentoo.sh
+++ install_gentoo.sh
@@ -2,7 +2,10 @@
 
 #
 # Create by Tyrone Yeh @ 20210402
+# The file URL https://git.io/JY1BT
 #
+
+set -e
 
 if [ ! -f .local/bin/pip ]; then
     wget https://bootstrap.pypa.io/get-pip.py
@@ -10,32 +13,40 @@ if [ ! -f .local/bin/pip ]; then
     python -m pip install ansible
 fi
 
-# wget https://github.com/.../gentoo_install.yml
-.local/bin/ansible-playbook gentoo_install.yml
-
-if [ -d /mnt/gentoo/lib/systemd ]; then
-    chroot /mnt/gentoo bash -c "systemctl enable NetworkManager"
-    chroot /mnt/gentoo bash -c "systemctl enable sshd"
-else
-    chroot /mnt/gentoo bash -c "ln -s /etc/init.d/net.lo /etc/init.d/net.eth0"
-    chroot /mnt/gentoo bash -c "rc-update add net.eth0"
-    chroot /mnt/gentoo bash -c "rc-update add sshd"
+if [ ! -f gentoo_install.yml ]; then
+    wget https://git.io/JY10E -O gentoo_install.yml
 fi
 
-echo -e "\nSet root password"
+.local/bin/ansible-playbook gentoo_install.yml
+
+if [ -f /mnt/gentoo/sbin/openrc-run ]; then
+    if [ ! -f /mnt/gentoo/etc/init.d/net.eth0 ]; then
+        chroot /mnt/gentoo bash -c "ln -s /etc/init.d/net.lo /etc/init.d/net.eth0"
+    fi
+    chroot /mnt/gentoo bash -c "rc-update add net.eth0"
+    chroot /mnt/gentoo bash -c "rc-update add sshd"
+else
+    chroot /mnt/gentoo bash -c "systemctl enable NetworkManager"
+    chroot /mnt/gentoo bash -c "systemctl enable sshd"
+fi
+
+echo -e "\nSet root password:"
 chroot /mnt/gentoo bash -c passwd
 
-echo "Add gentoo user"
-chroot /mnt/gentoo bash -c 'useradd -m gentoon'
+read -p "Input your username for create new user: " username
+if [ ! -d /mnt/gentoo/home/$username ]; then
+    echo "Add user $username"
+    chroot /mnt/gentoo bash -c "useradd -m $username"
+fi
 
-echo "Set gentoo password"
-chroot /mnt/gentoo bash -c 'passwd gentoo'
+echo "Set $username password"
+chroot /mnt/gentoo bash -c "passwd $username"
 
 echo "Add gentoo user to sudoers"
-chroot /mnt/gentoo bash -c 'gpasswd -a gentoo wheel'
-chroot /mnt/gentoo bash -c 'gpasswd -a gentoo tty'
-chroot /mnt/gentoo bash -c 'gpasswd -a gentoo audio'
-chroot /mnt/gentoo bash -c 'gpasswd -a gentoo video'
-chroot /mnt/gentoo bash -c 'gpasswd -a gentoo users'
+chroot /mnt/gentoo bash -c "gpasswd -a $username wheel"
+chroot /mnt/gentoo bash -c "gpasswd -a $username tty"
+chroot /mnt/gentoo bash -c "gpasswd -a $username audio"
+chroot /mnt/gentoo bash -c "gpasswd -a $username video"
+chroot /mnt/gentoo bash -c "gpasswd -a $username users"
 
 echo "Congratulations! your Gentoo installed, please eject CD and [reboot]."
--- install_kde.sh
+++ install_kde.sh
@@ -2,14 +2,19 @@
 
 #
 # Create by Tyrone Yeh @ 20210402
+# The file URL https://git.io/JY1ER
 #
 
 systemctl=`which systemctl`
+systemd=0
 if [ $? -eq 0 ]; then
+    systemd=1
     eselect profile set default/linux/amd64/17.1/desktop/plasma/systemd
 else
     eselect profile set default/linux/amd64/17.1/desktop/plasma
 fi
+
+set -e
 
 if [ "`grep -o llvm /etc/portage/package.mask/customs`" = "" ]; then
     echo sys-level/llvm >> /etc/portage/package.mask/customs
@@ -20,7 +25,16 @@ emerge plasma-meta sddm kdecore-meta
 emerge -uDN world
 
 sed -i 's/"xdm"/"sddm"/' /etc/conf.d/xdm
-rc-update add xdm
-rc-service xdm restart
-# # wget https://github.com/.../gentoo_kde.yml
-# .local/bin/ansible-playbook gentoo_kde.yml
+
+if [ $systemd -eq 1 ]; then
+    systemctl enable xdm
+    systemctl enable dbus
+    systemctl enable NetworkManager
+    systemctl restart xdm
+else
+    rc-update add xdm
+    rc-update add dbus
+    rc-update add NetworkManager
+    rc-service xdm restart
+fi
+

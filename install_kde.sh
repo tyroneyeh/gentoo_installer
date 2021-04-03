f65e4f27e4e01fd2f8abfc6a7f28c327004426e2
Tyrone Yeh <tyrone_yeh@draytek.com>
2021-04-03 23:19:58 +0800

    Fix systemd condition incorrect.

--- install_kde.sh
+++ install_kde.sh
@@ -7,7 +7,7 @@
 
 systemctl=`which systemctl`
 systemd=0
-if [ $? -eq 0 ]; then
+if [ $? -eq 1 ]; then
     systemd=1
     eselect profile set default/linux/amd64/17.1/desktop/plasma/systemd
 else
@@ -16,9 +16,15 @@ fi
 
 set -e
 
-if [ "`grep -o llvm /etc/portage/package.mask/customs`" = "" ]; then
+if [ ! -f /etc/portage/package.mask/customs -o "`grep -o llvm /etc/portage/package.mask/customs`" = "" ]; then
     echo sys-level/llvm >> /etc/portage/package.mask/customs
     echo sys-level/llvm-common >> /etc/portage/package.mask/customs
+fi
+
+if [ ! -f /etc/portage/package.use/customs -o "`grep -o libdrm`" = "" ]; then
+    echo x11-libs/libdrm libkms >> /etc/portage/package.use/customs
+    echo media-libs/mesa xa >> /etc/portage/package.use/customs
+    echo sys-apps/dbus user-session >> /etc/portage/package.use/customs
 fi
 
 emerge plasma-meta sddm kdecore-meta
